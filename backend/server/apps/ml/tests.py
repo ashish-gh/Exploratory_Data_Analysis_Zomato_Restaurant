78df23b4005565933a9eae7c5bc24dd2da134122
ashish ghimire <ashish.gh123@gmail.com>
2020-06-13 13:29:23 +0545

    Add ml code

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 
 import inspect
-from apps.ml.classifier.lstm import LstmClassifier
-from apps.ml.classifier.lstm import LstmClassifier
+from apps.ml.registry import MLRegistry
+
+from apps.ml.rate_classifier.lstm import LstmClassifier
+from apps.ml.rate_classifier.lstm import LstmClassifier
+
 
 class MLTest(TestCase):
 
@@ -21,3 +24,23 @@ class MLTest(TestCase):
         self.assertEqual('OK', response['status'])
         # self.assertTrue('rate' in response)
         # self.assertEqual(-0.0042696763, response['rate'])
+
+
+    # add below method to MLTests class:
+    def test_registry(self):
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "rate_classifier"
+        algorithm_object = LstmClassifier()
+        algorithm_name = "lstm"
+        algorithm_status = "production"
+        algorithm_version = "0.0.1"
+        algorithm_owner = "Ashish"
+        algorithm_description = "Lstm simple pre-preocessing"
+        algorithm_code = inspect.getsource(LstmClassifier)
+        # add to registry
+        registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                    algorithm_status, algorithm_version, algorithm_owner,
+                    algorithm_description, algorithm_code)
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
 application = get_wsgi_application()
+
+import inspect 
+from apps.ml.registry import MLRegistry
+from apps.ml.rate_classifier.lstm import LstmClassifier
+
+try:
+    registry = MLRegistry() # create ML registry
+    # LSTM
+    lstm = LstmClassifier()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="rate_classifier",
+                            algorithm_object=lstm,
+                            algorithm_name="lstm",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Ashish",
+                            algorithm_description="LSTM with simple pre and post processing",
+                            algorithm_code=inspect.getsource(LstmClassifier))
+
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
